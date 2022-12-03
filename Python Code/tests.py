f622f66b210b159fee96e4c8018140f6f5ff0101
sophiealtmann <s.altmann@mci4me.at>
2022-12-03 23:43:47 +0100

    plots

--- Python Code/tests.py
+++ Python Code/tests.py
@@ -25,173 +25,187 @@ mvc_env=fun.huelle(mvc_offset, mvc.t, mvc_gleich, 3)
 weights_env=fun.huelle(weights_offset, weights.t, weights_gleich, 3)
 fatigue_env=fun.huelle(fatigue_offset, fatigue.t, fatigue_gleich, 3)
 
-#mvc_s,mvc_e,weights_s,weights_e,fatigue_s,fatigue_e= lf3.get_bursts(mvc_filtered,weights_filtered,fatigue_filtered)
+# mvc_s,mvc_e,weights_s,weights_e,fatigue_s,fatigue_e= lf3.get_bursts(mvc_filtered,weights_filtered,fatigue_filtered)
+# print(mvc_s,mvc_e)
 
-mvc_s=[927,4940,8165]
-mvc_e=[3201,6977,10880]
+mvc_s=[1001,4913,8676]
+mvc_e=[3086,7246,10895]
 weights_s= [1091,6451,12449]
 weights_e= [4992,10863,16734]
 fatigue_s= [875,8695,16550]
 fatigue_e= [6957,14847,22354]
 
-burst1_start1=fatigue.t[fatigue_s[0]]+1000
-burst1_end1= burst1_start1+500
-burst1_start2= burst1_end1+7500
-burst1_end2= burst1_start2+500
-burst1_start3=fatigue.t[fatigue_e[0]]-1500
-burst1_end3= burst1_start3+500
+mvc_mean=[]
+weights_mean=[]
+fatigue_mean=[]
 
-burst2_start1=fatigue.t[fatigue_s[1]]+1000
-burst2_end1= burst2_start1+500
-burst2_start2= burst2_end1+7500
-burst2_end2= burst2_start2+500
-burst2_start3=fatigue.t[fatigue_e[1]]-1500
-burst2_end3= burst2_start3+500
+for i in range(3):
+   mvc_mean.append(np.mean(mvc_env[mvc_s[i]:mvc_e[i]]))
+   weights_mean.append(np.mean(weights_env[weights_s[i]:weights_e[i]]))
+   fatigue_mean.append(np.mean(fatigue_env[fatigue_s[i]:fatigue_e[i]]))
 
-burst3_start1=fatigue.t[fatigue_s[2]]+1000
-burst3_end1= burst3_start1+500
-burst3_start2= burst3_end1+7500
-burst3_end2= burst3_start2+500
-burst3_start3=fatigue.t[fatigue_e[2]]-1500
-burst3_end3= burst3_start3+500
+print (mvc_mean, weights_mean, fatigue_mean)
+real_mvc=np.mean(mvc_mean)
+print(real_mvc)
 
-burst1_index_s1=np.argmin(np.abs(fatigue.t - burst1_start1))
-burst1_index_e1=np.argmin(np.abs(fatigue.t - burst1_end1))
-burst1_index_s2=np.argmin(np.abs(fatigue.t - burst1_start2))
-burst1_index_e2=np.argmin(np.abs(fatigue.t - burst1_end2))
-burst1_index_s3=np.argmin(np.abs(fatigue.t - burst1_start3))
-burst1_index_e3=np.argmin(np.abs(fatigue.t - burst1_end3))
+# burst1_start1=fatigue.t[fatigue_s[0]]+1000
+# burst1_end1= burst1_start1+500
+# burst1_start2= burst1_end1+7500
+# burst1_end2= burst1_start2+500
+# burst1_start3=fatigue.t[fatigue_e[0]]-1500
+# burst1_end3= burst1_start3+500
 
-burst2_index_s1=np.argmin(np.abs(fatigue.t - burst2_start1))
-burst2_index_e1=np.argmin(np.abs(fatigue.t - burst2_end1))
-burst2_index_s2=np.argmin(np.abs(fatigue.t - burst2_start2))
-burst2_index_e2=np.argmin(np.abs(fatigue.t - burst2_end2))
-burst2_index_s3=np.argmin(np.abs(fatigue.t - burst2_start3))
-burst2_index_e3=np.argmin(np.abs(fatigue.t - burst2_end3))
+# burst2_start1=fatigue.t[fatigue_s[1]]+1000
+# burst2_end1= burst2_start1+500
+# burst2_start2= burst2_end1+7500
+# burst2_end2= burst2_start2+500
+# burst2_start3=fatigue.t[fatigue_e[1]]-1500
+# burst2_end3= burst2_start3+500
 
-burst3_index_s1=np.argmin(np.abs(fatigue.t - burst3_start1))
-burst3_index_e1=np.argmin(np.abs(fatigue.t - burst3_end1))
-burst3_index_s2=np.argmin(np.abs(fatigue.t - burst3_start2))
-burst3_index_e2=np.argmin(np.abs(fatigue.t - burst3_end2))
-burst3_index_s3=np.argmin(np.abs(fatigue.t - burst3_start3))
-burst3_index_e3=np.argmin(np.abs(fatigue.t - burst3_end3))
+# burst3_start1=fatigue.t[fatigue_s[2]]+1000
+# burst3_end1= burst3_start1+500
+# burst3_start2= burst3_end1+7500
+# burst3_end2= burst3_start2+500
+# burst3_start3=fatigue.t[fatigue_e[2]]-1500
+# burst3_end3= burst3_start3+500
 
-b1start_isolated= fatigue_filtered[burst1_index_s1:burst1_index_e1]
-b1mid_isolated= fatigue_filtered[burst1_index_s2:burst1_index_e2]
-b1end_isolated= fatigue_filtered[burst1_index_s3:burst1_index_e3]
+# burst1_index_s1=np.argmin(np.abs(fatigue.t - burst1_start1))
+# burst1_index_e1=np.argmin(np.abs(fatigue.t - burst1_end1))
+# burst1_index_s2=np.argmin(np.abs(fatigue.t - burst1_start2))
+# burst1_index_e2=np.argmin(np.abs(fatigue.t - burst1_end2))
+# burst1_index_s3=np.argmin(np.abs(fatigue.t - burst1_start3))
+# burst1_index_e3=np.argmin(np.abs(fatigue.t - burst1_end3))
 
-b2start_isolated= fatigue_filtered[burst2_index_s1:burst2_index_e1]
-b2mid_isolated= fatigue_filtered[burst2_index_s2:burst2_index_e2]
-b2end_isolated= fatigue_filtered[burst2_index_s3:burst2_index_e3]
+# burst2_index_s1=np.argmin(np.abs(fatigue.t - burst2_start1))
+# burst2_index_e1=np.argmin(np.abs(fatigue.t - burst2_end1))
+# burst2_index_s2=np.argmin(np.abs(fatigue.t - burst2_start2))
+# burst2_index_e2=np.argmin(np.abs(fatigue.t - burst2_end2))
+# burst2_index_s3=np.argmin(np.abs(fatigue.t - burst2_start3))
+# burst2_index_e3=np.argmin(np.abs(fatigue.t - burst2_end3))
 
-b3start_isolated= fatigue_filtered[burst3_index_s1:burst3_index_e1]
-b3mid_isolated= fatigue_filtered[burst3_index_s2:burst3_index_e2]
-b3end_isolated= fatigue_filtered[burst3_index_s3:burst3_index_e3]
+# burst3_index_s1=np.argmin(np.abs(fatigue.t - burst3_start1))
+# burst3_index_e1=np.argmin(np.abs(fatigue.t - burst3_end1))
+# burst3_index_s2=np.argmin(np.abs(fatigue.t - burst3_start2))
+# burst3_index_e2=np.argmin(np.abs(fatigue.t - burst3_end2))
+# burst3_index_s3=np.argmin(np.abs(fatigue.t - burst3_start3))
+# burst3_index_e3=np.argmin(np.abs(fatigue.t - burst3_end3))
 
-b1start_power,b1start_freq=lf3.get_power(b1start_isolated,1000)
-b1mid_power,b1mid_freq=lf3.get_power(b1mid_isolated,1000)
-b1end_power,b1end_freq=lf3.get_power(b1end_isolated,1000)
+# b1start_isolated= fatigue_filtered[burst1_index_s1:burst1_index_e1]
+# b1mid_isolated= fatigue_filtered[burst1_index_s2:burst1_index_e2]
+# b1end_isolated= fatigue_filtered[burst1_index_s3:burst1_index_e3]
 
-b2start_power,b2start_freq=lf3.get_power(b2start_isolated,1000)
-b2mid_power,b2mid_freq=lf3.get_power(b2mid_isolated,1000)
-b2end_power,b2end_freq=lf3.get_power(b2end_isolated,1000)
+# b2start_isolated= fatigue_filtered[burst2_index_s1:burst2_index_e1]
+# b2mid_isolated= fatigue_filtered[burst2_index_s2:burst2_index_e2]
+# b2end_isolated= fatigue_filtered[burst2_index_s3:burst2_index_e3]
 
-b3start_power,b3start_freq=lf3.get_power(b3start_isolated,1000)
-b3mid_power,b3mid_freq=lf3.get_power(b3mid_isolated,1000)
-b3end_power,b3end_freq=lf3.get_power(b3end_isolated,1000)
+# b3start_isolated= fatigue_filtered[burst3_index_s1:burst3_index_e1]
+# b3mid_isolated= fatigue_filtered[burst3_index_s2:burst3_index_e2]
+# b3end_isolated= fatigue_filtered[burst3_index_s3:burst3_index_e3]
 
-b1start_pwrfilt=fun.freqfilt(b1start_power)
-b1mid_pwrfilt=fun.freqfilt(b1mid_power)
-b1end_pwrfilt=fun.freqfilt(b1end_power)
+# b1start_power,b1start_freq=lf3.get_power(b1start_isolated,1000)
+# b1mid_power,b1mid_freq=lf3.get_power(b1mid_isolated,1000)
+# b1end_power,b1end_freq=lf3.get_power(b1end_isolated,1000)
 
-b2start_pwrfilt=fun.freqfilt(b2start_power)
-b2mid_pwrfilt=fun.freqfilt(b2mid_power)
-b2end_pwrfilt=fun.freqfilt(b2end_power)
+# b2start_power,b2start_freq=lf3.get_power(b2start_isolated,1000)
+# b2mid_power,b2mid_freq=lf3.get_power(b2mid_isolated,1000)
+# b2end_power,b2end_freq=lf3.get_power(b2end_isolated,1000)
 
-b3start_pwrfilt=fun.freqfilt(b3start_power)
-b3mid_pwrfilt=fun.freqfilt(b3mid_power)
-b3end_pwrfilt=fun.freqfilt(b3end_power)
+# b3start_power,b3start_freq=lf3.get_power(b3start_isolated,1000)
+# b3mid_power,b3mid_freq=lf3.get_power(b3mid_isolated,1000)
+# b3end_power,b3end_freq=lf3.get_power(b3end_isolated,1000)
 
-b1start_median=fun.getmedian(b1start_power,b1start_freq)
-b1mid_median=fun.getmedian(b1mid_power,b1mid_freq)
-b1end_median=fun.getmedian(b1end_power,b1end_freq)
+# b1start_pwrfilt=fun.freqfilt(b1start_power)
+# b1mid_pwrfilt=fun.freqfilt(b1mid_power)
+# b1end_pwrfilt=fun.freqfilt(b1end_power)
 
-b2start_median=fun.getmedian(b2start_power,b2start_freq)
-b2mid_median=fun.getmedian(b2mid_power,b2mid_freq)
-b2end_median=fun.getmedian(b2end_power,b2end_freq)
+# b2start_pwrfilt=fun.freqfilt(b2start_power)
+# b2mid_pwrfilt=fun.freqfilt(b2mid_power)
+# b2end_pwrfilt=fun.freqfilt(b2end_power)
 
-b3start_median=fun.getmedian(b3start_power,b3start_freq)
-b3mid_median=fun.getmedian(b3mid_power,b3mid_freq)
-b3end_median=fun.getmedian(b3end_power,b3end_freq)
+# b3start_pwrfilt=fun.freqfilt(b3start_power)
+# b3mid_pwrfilt=fun.freqfilt(b3mid_power)
+# b3end_pwrfilt=fun.freqfilt(b3end_power)
 
-plt.plot(b1start_freq,b1start_power/10,label='Unfiltered Power')
-plt.plot(b1start_freq,b1start_pwrfilt/10,color='red',label='Filtered Power')
-plt.axvline(x=b1start_median,color='green',label='Median')
-plt.xlabel('Frequency(Hz)')
-plt.ylabel('Power(dB)')
-plt.legend(loc="best",frameon=True)
-plt.show()
+# b1start_median=fun.getmedian(b1start_power,b1start_freq)
+# b1mid_median=fun.getmedian(b1mid_power,b1mid_freq)
+# b1end_median=fun.getmedian(b1end_power,b1end_freq)
 
-plt.plot(b1mid_freq,b1mid_power/10,label='Unfiltered Power')
-plt.plot(b1mid_freq,b1mid_pwrfilt/10,color='red',label='Filtered Power')
-plt.axvline(x=b1mid_median,color='green',label='Median')
-plt.xlabel('Frequency(Hz)')
-plt.ylabel('Power(dB)')
-plt.legend(loc="best",frameon=True)
-plt.show()
+# b2start_median=fun.getmedian(b2start_power,b2start_freq)
+# b2mid_median=fun.getmedian(b2mid_power,b2mid_freq)
+# b2end_median=fun.getmedian(b2end_power,b2end_freq)
 
-plt.plot(b1end_freq,b1end_power/10,label='Unfiltered Power')
-plt.plot(b1end_freq,b1end_pwrfilt/10,color='red',label='Filtered Power')
-plt.axvline(x=b1end_median,color='green',label='Median')
-plt.xlabel('Frequency(Hz)')
-plt.ylabel('Power(dB)')
-plt.legend(loc="best",frameon=True)
-plt.show()
+# b3start_median=fun.getmedian(b3start_power,b3start_freq)
+# b3mid_median=fun.getmedian(b3mid_power,b3mid_freq)
+# b3end_median=fun.getmedian(b3end_power,b3end_freq)
 
-plt.plot(b2start_freq,b2start_power/10,label='Unfiltered Power')
-plt.plot(b2start_freq,b2start_pwrfilt/10,color='red',label='Filtered Power')
-plt.axvline(x=b2start_median,color='green',label='Median')
-plt.xlabel('Frequency(Hz)')
-plt.ylabel('Power(dB)')
-plt.legend(loc="best",frameon=True)
-plt.show()
+# plt.plot(b1start_freq,b1start_power/10,label='Unfiltered Power')
+# plt.plot(b1start_freq,b1start_pwrfilt/10,color='red',label='Filtered Power')
+# plt.axvline(x=b1start_median,color='green',label='Median')
+# plt.xlabel('Frequency(Hz)')
+# plt.ylabel('Power(dB)')
+# plt.legend(loc="best",frameon=True)
+# plt.show()
 
-plt.plot(b2mid_freq,b2mid_power/10,label='Unfiltered Power')
-plt.plot(b2mid_freq,b2mid_pwrfilt/10,color='red',label='Filtered Power')
-plt.axvline(x=b2mid_median,color='green',label='Median')
-plt.xlabel('Frequency(Hz)')
-plt.ylabel('Power(dB)')
-plt.legend(loc="best",frameon=True)
-plt.show()
+# plt.plot(b1mid_freq,b1mid_power/10,label='Unfiltered Power')
+# plt.plot(b1mid_freq,b1mid_pwrfilt/10,color='red',label='Filtered Power')
+# plt.axvline(x=b1mid_median,color='green',label='Median')
+# plt.xlabel('Frequency(Hz)')
+# plt.ylabel('Power(dB)')
+# plt.legend(loc="best",frameon=True)
+# plt.show()
 
-plt.plot(b2end_freq,b2end_power/10,label='Unfiltered Power')
-plt.plot(b2end_freq,b2end_pwrfilt/10,color='red',label='Filtered Power')
-plt.axvline(x=b2end_median,color='green',label='Median')
-plt.xlabel('Frequency(Hz)')
-plt.ylabel('Power(dB)')
-plt.legend(loc="best",frameon=True)
-plt.show()
+# plt.plot(b1end_freq,b1end_power/10,label='Unfiltered Power')
+# plt.plot(b1end_freq,b1end_pwrfilt/10,color='red',label='Filtered Power')
+# plt.axvline(x=b1end_median,color='green',label='Median')
+# plt.xlabel('Frequency(Hz)')
+# plt.ylabel('Power(dB)')
+# plt.legend(loc="best",frameon=True)
+# plt.show()
 
-plt.plot(b3start_freq,b3start_power/10,label='Unfiltered Power')
-plt.plot(b3start_freq,b3start_pwrfilt/10,color='red',label='Filtered Power')
-plt.axvline(x=b3start_median,color='green',label='Median')
-plt.xlabel('Frequency(Hz)')
-plt.ylabel('Power(dB)')
-plt.legend(loc="best",frameon=True)
-plt.show()
+# plt.plot(b2start_freq,b2start_power/10,label='Unfiltered Power')
+# plt.plot(b2start_freq,b2start_pwrfilt/10,color='red',label='Filtered Power')
+# plt.axvline(x=b2start_median,color='green',label='Median')
+# plt.xlabel('Frequency(Hz)')
+# plt.ylabel('Power(dB)')
+# plt.legend(loc="best",frameon=True)
+# plt.show()
 
-plt.plot(b3mid_freq,b3mid_power/10,label='Unfiltered Power')
-plt.plot(b3mid_freq,b3mid_pwrfilt/10,color='red',label='Filtered Power')
-plt.axvline(x=b3mid_median,color='green',label='Median')
-plt.xlabel('Frequency(Hz)')
-plt.ylabel('Power(dB)')
-plt.legend(loc="best",frameon=True)
-plt.show()
+# plt.plot(b2mid_freq,b2mid_power/10,label='Unfiltered Power')
+# plt.plot(b2mid_freq,b2mid_pwrfilt/10,color='red',label='Filtered Power')
+# plt.axvline(x=b2mid_median,color='green',label='Median')
+# plt.xlabel('Frequency(Hz)')
+# plt.ylabel('Power(dB)')
+# plt.legend(loc="best",frameon=True)
+# plt.show()
 
-plt.plot(b3end_freq,b3end_power/10,label='Unfiltered Power')
-plt.plot(b3end_freq,b3end_pwrfilt/10,color='red',label='Filtered Power')
-plt.axvline(x=b3end_median,color='green',label='Median')
-plt.xlabel('Frequency(Hz)')
-plt.ylabel('Power(dB)')
-plt.legend(loc="best",frameon=True)
-plt.show()
+# plt.plot(b2end_freq,b2end_power/10,label='Unfiltered Power')
+# plt.plot(b2end_freq,b2end_pwrfilt/10,color='red',label='Filtered Power')
+# plt.axvline(x=b2end_median,color='green',label='Median')
+# plt.xlabel('Frequency(Hz)')
+# plt.ylabel('Power(dB)')
+# plt.legend(loc="best",frameon=True)
+# plt.show()
+
+# plt.plot(b3start_freq,b3start_power/10,label='Unfiltered Power')
+# plt.plot(b3start_freq,b3start_pwrfilt/10,color='red',label='Filtered Power')
+# plt.axvline(x=b3start_median,color='green',label='Median')
+# plt.xlabel('Frequency(Hz)')
+# plt.ylabel('Power(dB)')
+# plt.legend(loc="best",frameon=True)
+# plt.show()
+
+# plt.plot(b3mid_freq,b3mid_power/10,label='Unfiltered Power')
+# plt.plot(b3mid_freq,b3mid_pwrfilt/10,color='red',label='Filtered Power')
+# plt.axvline(x=b3mid_median,color='green',label='Median')
+# plt.xlabel('Frequency(Hz)')
+# plt.ylabel('Power(dB)')
+# plt.legend(loc="best",frameon=True)
+# plt.show()
+
+# plt.plot(b3end_freq,b3end_power/10,label='Unfiltered Power')
+# plt.plot(b3end_freq,b3end_pwrfilt/10,color='red',label='Filtered Power')
+# plt.axvline(x=b3end_median,color='green',label='Median')
+# plt.xlabel('Frequency(Hz)')
+# plt.ylabel('Power(dB)')
+# plt.legend(loc="best",frameon=True)
+# plt.show()
